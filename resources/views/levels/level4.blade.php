03ffa6b73ead0b8f4d185d70d9cd71c9cf6e4176
ghvedashvili <ghvedashvili@gmail.com>
2026-03-08 00:35:38 +0400

    added levelcomlete text

--- resources/views/levels/level4.blade.php
+++ resources/views/levels/level4.blade.php
@@ -61,22 +61,8 @@ Swal.fire({
 </script>
 @endif
    
- @else
-                       <div class="container mt-5 pt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-body text-center">
-                    <h5 class="card-title">level{{ $level }}</h5>
-                    @if($userLevel > $level)
-                        <div class="alert alert-success">მადლობა გამოწერისთვის!</div>
-                    @else
-                        <div class="alert alert-warning">⚠️ ეს დონე ჯერ არ არის ხელმისაწვდომი</div>
-                    @endif
-                    <a href="{{ route('levels.show', ['level' => $userLevel]) }}" class="btn btn-primary">გადადით მიმდინარე დონეზე</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+@else
+
+@include('levels.levelcomplete', ['level' => $level,  'userLevel' => auth()->user()->level])
+
 @endif
--- resources/views/levels/level6.blade.php
+++ resources/views/levels/level6.blade.php
@@ -445,23 +445,7 @@
 </script>
 
 @else
-  <div class="container mt-5 pt-5">
-    <div class="row justify-content-center">
-      <div class="col-md-6">
-        <div class="card">
-          <div class="card-body text-center">
-            <h5 class="card-title">level{{ $level }}</h5>
-            @if($userLevel > $level)
-              <div class="alert alert-success">დიახ რუსეთი ოკუპანტია!!!</div>
-            @else
-              <div class="alert alert-warning">⚠️ ეს დონე ჯერ არ არის ხელმისაწვდომი</div>
-            @endif
-            <a href="{{ route('levels.show', ['level' => $userLevel]) }}" class="btn btn-primary">
-              გადადით მიმდინარე დონეზე
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+
+@include('levels.levelcomplete', ['level' => $level,  'userLevel' => auth()->user()->level])
+
 @endif
