fc60bac3867b07ea5688ef2cbe36b44216f0c558
ghvedashvili <ghvedashvili@gmail.com>
2026-06-05 14:19:59 +0400

    ლოგო გაკეთდა

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="manifest" href="/manifest.json">
+<link rel="apple-touch-icon" href="/icons/apple-touch-icon.png">
 <meta name="theme-color" content="#080808">
     <!-- Preconnect hints -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
